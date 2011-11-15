314ceb24db032ba58e1beb5cf5638ac359b9effb
Piotr Niełacny <piotr.nielacny@gmail.com>
2011-11-15 12:17:26 +0100

    Initial commit

---
+++ lib/muzang-plugins.rb
@@ -0,0 +1,7 @@
+require "muzang-plugins/version"
+
+module Muzang
+  module Plugins
+    # Your code goes here...
+  end
+end
---
+++ lib/muzang-plugins/version.rb
@@ -0,0 +1,5 @@
+module Muzang
+  module Plugins
+    VERSION = "0.0.1"
+  end
+end
