e1203d65ea707db80d8e5c6c0574cdd5a434ccc3
Piotr Niełacny <piotr.nielacny@gmail.com>
2011-11-20 10:50:49 +0100

    Update rubygems plugin

--- lib/muzang-plugins/muzang-rubygems.rb
+++ lib/muzang-plugins/muzang-rubygems.rb
@@ -1,40 +1,69 @@
 require "em-http-request"
 require "json"
+require "pstore"
 
 class RubyGems
   include Muzang::Plugins::Helpers
 
-  attr_accessor :last_gem
+  attr_accessor :last_gem, :store
 
   def initialize(bot)
     @bot = bot
-    create_database("last_gem.yml", String.new, :last_gem)
-  end
-
-  def call(connection, message)
-    if on_join?(connection, message)
-      EventMachine.add_periodic_timer(period) do
-        http = EventMachine::HttpRequest.new('http://rubygems.org/api/v1/gems/latest.json').get
-
-        http.errback {
-          # exceptioner
-        }
-
-        http.callback {
-          gems = JSON.parse(http.response)
-          gem = gems.first
-          unless @last_gem == gem["name"]
-            @last_gem = gem["name"]
-            connection.msg(message.channel, "Nowy gem! | #{gem["name"]} | #{gem["info"]} | #{gem["project_uri"]}")
-            save
-          end
-        }
-      end
+    @store = PStore.new("#{ENV["HOME"]}/.muzang/muzang.rubygems")
+    @store.transaction do
+      @store[:gems] ||= {}
     end
   end
 
-  def save
-    file = File.open(@config + "/last_gem.yml", "w"){|f| f.write YAML::dump(@last_gem)}
+  def call(connection, message)
+    message.message.match(/^watch! (.*?)$/) do |match|
+      @current_gem = match[1]
+
+      @store.transaction do
+        unless @store[:gems][@current_gem]
+          @store[:gems][match[1]] = {:name => @current_gem}
+          @new_gem = true
+        end
+      end
+
+      if @new_gem
+        http = EventMachine::HttpRequest.new("http://rubygems.org/api/v1/gems/#{@current_gem}.json").get
+        http.callback {
+          begin
+            gem = JSON.parse(http.response)
+            @store.transaction do
+              @store[:gems][@current_gem][:version] = gem["version"]
+            end
+            connection.msg(message.channel, "Dodalem gema #{@current_gem} do obserwowanych")
+            connection.msg(message.channel, "Aktualna wersja: #{@current_gem} (#{gem["version"]})")
+            @new_gem = false
+          rescue Exception
+            connection.msg(message.channel, "Niepoprawna nazwa gema")
+          end
+        }
+      else
+        connection.msg(message.channel, "Gem #{@current_gem} jest juz obserwowany")
+      end
+    end
+
+    if on_join?(connection, message)
+      EventMachine.add_periodic_timer(period) do
+        gems = @store.transaction{@store[:gems].values}
+        EM::Iterator.new(gems, 1).each do |gem, iter|
+          http = EventMachine::HttpRequest.new("http://rubygems.org/api/v1/gems/#{gem[:name]}.json").get
+          http.callback {
+            iter.next
+            current_gem = JSON.parse(http.response)
+            if Gem::Version.new(gem[:version]) < Gem::Version.new(current_gem["version"])
+              @store.transaction do
+                @store[:gems][gem[:name]][:version] = current_gem["version"]
+              end
+              connection.msg(message.channel, "Nowa wersja #{gem[:name]} (#{current_gem["version"]})")
+            end
+          }
+        end
+      end
+    end
   end
 
   def period
--- spec/rubygems_spec.rb
+++ spec/rubygems_spec.rb
@@ -12,26 +12,64 @@ describe "RubyGems" do
     @bot = stub
     @rubygems = RubyGems.new(@bot)
     @connection = ConnectionMock.new(:nick => "DRUG-bot")
-    @message = OpenStruct.new({ :command => :join, :channel => "#test", :nick => "DRUG-bot" })
-    @file = File.expand_path('../support/responses/rubygems.response', __FILE__)
-    EventMachine::MockHttpRequest.pass_through_requests = false
-    EventMachine::MockHttpRequest.register_file('http://rubygems.org:80/api/v1/gems/latest.json', :get, @file)
+    @message = OpenStruct.new({ :command => :prvmsg, :channel => "#test", :nick => "DRUG-bot" })
+    @gem = File.expand_path('../support/responses/acts-as-messageable.response', __FILE__)
+    @gem2 = File.expand_path('../support/responses/acts-as-messageable2.response', __FILE__)
+    EventMachine::MockHttpRequest.pass_through_requests = true
+    EventMachine::MockHttpRequest.register_file('http://rubygems.org:80/api/v1/gems/acts-as-messageable.json', :get, @gem)
+    EventMachine::MockHttpRequest.register_file('http://rubygems.org:80/api/v1/gems/acts-as-messageable2.json', :get, @gem2)
     EventMachine::MockHttpRequest.activate!
   end
 
-  it "should print last gem" do
-    @rubygems.last_gem = "fake_gem"
+  after do
+    FileUtils.rm("#{ENV["HOME"]}/.muzang/muzang.rubygems")
+  end
+
+  it "should add gem to watching list" do
+    @message.message = "watch! acts-as-messageable"
     EM.run do
       @rubygems.call(@connection, @message)
-      eventually(1) { @connection.message_count }
+      eventually(true, :times => 10) do
+        @connection.messages.include? "Dodalem gema acts-as-messageable do obserwowanych" and
+        @connection.messages.include? "Aktualna wersja: acts-as-messageable (0.4.2)"
+      end
     end
   end
 
-  it "should not print last gem" do
-    @rubygems.last_gem = "action_links"
+  it "should raise error" do
+    @message.message = "watch! acts-as-messageable2"
     EM.run do
       @rubygems.call(@connection, @message)
-      eventually(0) { @connection.message_count }
+      eventually(true) do
+        @connection.messages.include? "Niepoprawna nazwa gema"
+      end
+    end
+  end
+
+  it "should not add gem twice to store" do
+    @message.message = "watch! acts-as-messageable"
+    EM.run do
+      2.times{ @rubygems.call(@connection, @message) }
+      eventually(true) do
+        @connection.messages.include? "Gem acts-as-messageable jest juz obserwowany"
+      end
+    end
+  end
+
+  it "should notice about new version of gem" do
+    @message.message = ""
+    @message.command = :join
+
+    EM.run do
+      @rubygems.call(@connection, @message)
+      @message.message = "watch! acts-as-messageable"
+      @rubygems.call(@connection, @message)
+
+      @rubygems.store.transaction{ @rubygems.store[:gems]["acts-as-messageable"][:version] = "0.4.1" }
+
+      eventually(true, :every => 0.1, :total => 100) do
+        @connection.messages.include? "Nowa wersja acts-as-messageable (0.4.2)"
+      end
     end
   end
 end
